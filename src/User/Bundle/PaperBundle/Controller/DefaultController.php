7c081647eee12af341c6471dbe2217e413bd13af
jingjingxyk <464819862@qq.com>
2014-10-04 00:53:56 +0800

    添加sqlite数据库配置

--- src/User/Bundle/PaperBundle/Controller/DefaultController.php
+++ src/User/Bundle/PaperBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace User\Bundle\PaperBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction(Request $request )
     {
-        return $this->render('UserPaperBundle:Default:index.html.twig', array('name' => $name));
+    //	echo "<pre>" ;
+    //	var_dump($request);
+    	$conn = $this->get('doctrine.dbal.default_connection');
+    	$users = $conn->fetchAll('SELECT * FROM user');
+    	var_dump($users);
+        return $this->render('UserPaperBundle:Default:index.html.twig', array('name' => "xieyaokun"));
     }
 }

d9627e8cb589d08533a38d7519b0ca3e867fdff5
jingjingxyk <464819862@qq.com>
2014-11-03 08:29:02 +0800

    移动模板文件

--- src/User/Bundle/PaperBundle/Controller/DefaultController.php
+++ src/User/Bundle/PaperBundle/Controller/DefaultController.php
@@ -7,13 +7,10 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     public function indexAction(Request $request )
-    {
-    //	echo "<pre>" ;
-    //	var_dump($request);
+    {  
     
     	$conn = $this->get('doctrine.dbal.default_connection');
-    	$users = $conn->fetchAll('SELECT * FROM user');
-    	var_dump($users);
+    	$users = $conn->fetchAll('SELECT * FROM user');    
         return $this->render('UserPaperBundle:Default:index.html.twig', array('name' => "xieyaokun"));
     }
     public function loginAction(Request $request){
---
+++ src/User/Bundle/PaperBundle/Controller/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace User\Bundle\PaperBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{
+	public function indexAction(Request $request){
+		//return $this->render("UserPaperBundle:admin:login.html.twig");
+	}
+	
+}
